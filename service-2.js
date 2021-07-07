a2856c48592d5b9834ad5b5d95f6ca013e9a24c2
Massimo Iacolare <info@massimoiacolare.it>
2021-07-07 17:21:29 +0200

    use withSpan within service-2

--- service-2.js
+++ service-2.js
@@ -1,27 +1,20 @@
 // NOTE: this must be the first line you import
 require("./tracing").initTelemetry("service-2")
 
-const otelApi = require("@opentelemetry/api")
 const express = require("express")
+const { withSpan } = require("./otel-helpers")
 const { doWork } = require("./do-work")
 
 const app = express()
 const port = 3001
 
-const tracer = otelApi.trace.getTracer("service-2")
-
-app.get("/", async function (req, res) {
-    const span = tracer.startSpan("service-2|get /")
-    const ctx = otelApi.trace.setSpan(otelApi.context.active(), span)
-
-    const duration = await otelApi.context.with(ctx, async () => {
-        return await doWork()
-    })
-
-    res.send({ hello: "World", duration })
-
-    span.end()
-})
+app.get(
+    "/",
+    withSpan("service-2", "GET /", async function (req, res) {
+        const duration = await doWork()
+        res.send({ hello: "World", duration })
+    }),
+)
 
 app.listen(port, () => {
     console.log(`Service-2 listening at http://localhost:${port}`)
